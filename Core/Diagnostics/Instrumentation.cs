0600f91f56bcd37786e98afc5a0c697ee5292ae2
tr00d <guillaume.faas@live.fr>
2026-07-31 11:11:16 +0200

    chore: remove unused using directives

--- Core/Diagnostics/Instrumentation.cs
+++ Core/Diagnostics/Instrumentation.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
 using System.Reflection;
--- Core/Helpers.cs
+++ Core/Helpers.cs
@@ -1,8 +1,6 @@
 ﻿using Supabase.Core.Attributes;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Supabase.Core
 {
