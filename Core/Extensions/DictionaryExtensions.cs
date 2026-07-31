7e87034d2e8f71b25db4dae57327afd6d0d20aa6
tr00d <guillaume.faas@live.fr>
2026-07-31 11:18:14 +0200

    chore: use var when possible

--- Core/Extensions/DictionaryExtensions.cs
+++ Core/Extensions/DictionaryExtensions.cs
@@ -10,11 +10,11 @@ namespace Supabase.Core.Extensions
     {
         /// <summary>
         /// Merges two dictionaries, allowing overwrite priorities leftward.
-        /// 
+        ///
         /// Works in C#3/VS2008:
         /// Returns a new dictionary of this ... others merged leftward.
         /// Keeps the type of 'this', which must be default-instantiable.
-        /// Example: 
+        /// Example:
         ///   result = map.MergeLeft(other1, other2, ...)
         /// From: https://stackoverflow.com/a/2679857/3629438
         /// </summary>
@@ -27,10 +27,10 @@ namespace Supabase.Core.Extensions
         public static T MergeLeft<T, K, V>(this T me, params IDictionary<K, V>[] others)
             where T : IDictionary<K, V>, new()
         {
-            T newMap = new T();
-            foreach (IDictionary<K, V> src in (new List<IDictionary<K, V>> { me }).Concat(others))
+            var newMap = new T();
+            foreach (var src in (new List<IDictionary<K, V>> { me }).Concat(others))
             {
-                foreach (KeyValuePair<K, V> p in src)
+                foreach (var p in src)
                 {
                     newMap[p.Key] = p.Value;
                 }
